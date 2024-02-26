25cda3d9e31af00368045bfc896e9d3fe49112df
addison atkin <addisonathome@gmail.com>
2024-02-26 14:39:26 -0600

    added cookie disclaimer on home page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,33 @@
 const startSplitting = () => {
     window.location.href = 'split.html'
 }
+
+
+
+//privacy policy
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if the user has already accepted the policy
+    const hasConsented = localStorage.getItem('userConsent');
+    
+    if (!hasConsented) {
+      const modal = document.getElementById('privacyPolicyModal');
+      const span = document.getElementsByClassName('close')[0];
+      const acceptButton = document.getElementById('acceptPrivacyPolicy');
+  
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+  
+      // When the user clicks on "I Agree", close the modal and save consent
+      acceptButton.onclick = function() {
+        modal.style.display = "none";
+        localStorage.setItem('userConsent', 'true');
+      }
+  
+      // Show the modal
+      modal.style.display = "block";
+    }
+  });
+  
